91b7fa9074147465d536c6bed333e863afb0dc4d
samuelthaiyil <sathaiyi@lakeheadu.ca>
2025-11-30 15:36:10 -0500

    restructured fetching logic for readability

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import TooltipEditor, { Client } from "@/components/TooltipEditor";
+import TooltipEditor from "@/components/TooltipEditor";
+import { ClientCursorPosition } from "@/types/cursor";
 import { useDebounce } from "@/hooks/useDebounce";
 import { useWebSocket } from "@/hooks/useWebsocket";
 import { useUser } from "@clerk/nextjs";
@@ -13,60 +14,60 @@ export default function Home() {
   const { isConnected, messages, sendMessage } = useWebSocket(
     "ws://localhost:3001"
   );
-  const [clients, setClients] = useState<Client[]>([]);
-  const { isSignedIn, user } = useUser();
-  const router  = useRouter();
+  const [clientCursorPositions, setClientCursorPositions] = useState<
+    ClientCursorPosition[]
+  >([]);
+  const { isSignedIn } = useUser();
+  const router = useRouter();
 
   useEffect(() => {
-    const updateClients = (): void => {
-      if (!messages[0]) {
-        return;
-      }
+    if (!messages[0]) {
+      return;
+    }
 
-      const latestMessage = JSON.parse(messages[0]);
+    const addClientCursorPosition = (
+      clientCursorPosition: ClientCursorPosition
+    ) => {
+      setClientCursorPositions((prev) => [clientCursorPosition, ...prev]);
+      return;
+    };
 
-      const existingClient = clients.find(
-        (client) => client.id === latestMessage.clientId
-      );
-
-      if (!existingClient) {
-        setClients((prev) => [
-          {
-            id: latestMessage.clientId,
-            position: { top: latestMessage.top, left: latestMessage.left },
-          },
-          ...prev,
-        ]);
-        return;
-      }
-
-      setClients((prev) => {
-        return prev.map((client) => {
-          if (client.id === latestMessage.clientId) {
+    const updateClientCursorPosition = (
+      clientCursorPosition: ClientCursorPosition
+    ) => {
+      setClientCursorPositions((prev) => {
+        return prev.map((ccp) => {
+          if (ccp.id === latestMessage.id) {
             return {
-              ...client,
+              ...ccp,
               position: {
-                left: latestMessage.left,
-                top: latestMessage.top,
+                ...clientCursorPosition.position,
               },
             };
           }
-          return client;
+          return ccp;
         });
       });
     };
 
-    updateClients();
+    const latestMessage = JSON.parse(messages[0]);
+    const existingClient = clientCursorPositions.find(
+      (client) => client.id === latestMessage.id
+    );
+
+    existingClient
+      ? updateClientCursorPosition(latestMessage)
+      : addClientCursorPosition(latestMessage);
   }, [messages]);
 
   if (!isSignedIn) {
-    router.push('/signin');
+    router.push("/signin");
   }
 
   return (
     <div className="p-8 flex justify-center">
       <TooltipEditor
-        clients={clients}
+        clientCursorPositions={clientCursorPositions}
         value={value}
         onValueChange={setValue}
         hasKeyDownTimeoutReached={hasTimeoutReached}
