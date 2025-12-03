a98167820ded32f357dc10e35e841b0a2c0d5f8d
samuelthaiyil <sathaiyi@lakeheadu.ca>
2025-12-02 19:04:06 -0500

    added notebooks and settings routes

---
+++ frontend/app/notebooks/page.tsx
@@ -0,0 +1,7 @@
+export default function Notebooks() {
+  return (
+    <div className="p-8">
+      <h1 className="tracking-tighter text-2xl">Notebooks</h1>
+    </div>
+  );
+}
---
+++ frontend/app/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+    return (
+      <div className="p-8">
+        <h1 className="tracking-tighter text-2xl">Settings</h1>
+      </div>
+    );
+  }
+  
