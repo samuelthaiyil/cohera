4d6520c8d0cf14e045268c58e643f32d5f8339d8
samuelthaiyil <sathaiyi@lakeheadu.ca>
2025-12-05 16:38:25 -0500

    added convex

---
+++ frontend/convex/_generated/schema.ts
@@ -0,0 +1,11 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  notebooks: defineTable({
+    ownerUserId: v.string(),
+    name: v.string(),
+    content: v.string(),
+    invitedUserIds: v.array()
+  }),
+});
