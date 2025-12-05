f9fcee7b202d82f2140ed174dd662e9693504b30
samuelthaiyil <sathaiyi@lakeheadu.ca>
2025-12-05 16:37:22 -0500

    added notebooks table

--- frontend/app/notebooks/page.tsx
+++ frontend/app/notebooks/page.tsx
@@ -1,7 +1,28 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function Notebooks() {
   return (
     <div className="p-8">
-      <h1 className="tracking-tighter text-2xl">Notebooks</h1>
+      <h1 className="tracking-tighter text-2xl mb-6">Notebooks</h1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Created</TableHead>
+            <TableHead>Last Modified</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+        </TableBody>
+      </Table>
     </div>
   );
 }
