b23918385f39aae0836f4cc0e9120b9f76886586
samuelthaiyil <sathaiyi@lakeheadu.ca>
2025-12-05 20:39:45 -0500

    styled ui

--- frontend/app/notebooks/page.tsx
+++ frontend/app/notebooks/page.tsx
@@ -8,21 +8,23 @@ import {
 
 export default function Notebooks() {
   return (
-    <div className="p-8">
-      <h1 className="tracking-tighter text-2xl mb-6">Notebooks</h1>
+    <div className="p-8 w-full">
+      <div className="flex flex-row justify-between">
+          <h1 className="tracking-tighter text-2xl mb-6">Notebooks</h1>
+          <button className="bg-blue-500 px-4 py-1 text-white rounded-md hover:cursor-pointer">Create</button>
+      </div>
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Created</TableHead>
-            <TableHead>Last Modified</TableHead>
-            <TableHead className="text-right">Status</TableHead>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead className="w-[100px]">By</TableHead>
+            <TableHead className="w-[100px]">Date</TableHead>
+            <TableHead className="w-[100px]">Last Modified</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
-        </TableBody>
+        <TableBody></TableBody>
       </Table>
     </div>
   );
 }
+
